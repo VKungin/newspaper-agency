5129ecf26c13ac984cfeabe3e852f535674b8290
Vyacheslav Kungin <acheros4@gmail.com>
2023-05-05 18:55:30 +0300

    add home page

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from agency.models import Topic, Newspaper, Redactor
+
+
+def index(request):
+
+    num_topics = Topic.objects.count()
+    num_news = Newspaper.objects.count()
+    num_redactors = Redactor.objects.count()
+
+    context = {
+        "num_topics": num_topics,
+        "num_news": num_news,
+        "num_redactors": num_redactors,
+    }
+    return render(request, "agency/index.html", context=context)
