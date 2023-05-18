9d589c83a9f8c46a43e25e562d1253d816159ee4
Vyacheslav Kungin <acheros4@gmail.com>
2023-05-18 18:20:49 +0300

    staticfiles managing

--- NewsPaperAgency/settings.py
+++ NewsPaperAgency/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -99,7 +100,6 @@ DATABASES = {
 db_from_env = dj_database_url.config(
     conn_max_age=500,
 )
-
 DATABASES["default"].update(db_from_env)
 
 
@@ -141,7 +141,12 @@ USE_TZ = True
 
 STATIC_URL = "agency.static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / "static/",
+]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
