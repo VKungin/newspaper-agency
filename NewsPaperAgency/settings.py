ab211cea338c5b1fe1e46cfbb345dbe56b077e45
Vyacheslav Kungin <acheros4@gmail.com>
2023-05-22 14:56:25 +0300

    add "newspaper-agency-er0f.onrender.com" to ALLOWED_HOSTS

--- NewsPaperAgency/settings.py
+++ NewsPaperAgency/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # DEBUG = True
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "newspaper-agency-er0f.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
